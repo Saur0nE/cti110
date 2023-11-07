72c2ba65e5d092d1914d170f79773a30d3d2e16a
Ryan <150168712+Saur0nE@users.noreply.github.com>
2023-11-07 11:21:51 -0500

    First Upload

---
+++ p1lab1_saurondonryanjames.py
@@ -0,0 +1,9 @@
+#Saurondon Ryanjames
+#2023/11/07
+#Basic Input/Output
+
+#Get Input from user
+name = input("Please enter your name: ")
+
+#Display output to user
+print("Hello", name, "welcome to CTI-110")
